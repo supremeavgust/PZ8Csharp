41abe149c18eea1a23b62c9c49e055980e087a35
Stanislav <avgustwot@mail.ru>
2023-04-02 15:33:09 +0300

    Задача 67

---
+++ example67/Program.cs
@@ -0,0 +1,18 @@
+﻿// 67 Напишите программу, которая будет
+// принимать на вход число и возвращать сумму его цифр.
+
+int SummDigital(int number)
+{
+    if (number == 0)
+    {
+        return number;
+
+    }
+    return number % 10 + SummDigital(number / 10);
+
+}
+Console.Clear();
+Console.Write("Vvedite chislo: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SummDigital(number));
