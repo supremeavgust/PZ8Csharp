d376fa2b3f24419e77faf916c6b71e25824fa431
Stanislav <avgustwot@mail.ru>
2023-04-02 15:32:19 +0300

    Задача 63

---
+++ example63/Program.cs
@@ -0,0 +1,17 @@
+﻿// ЗАДАЧА 63 Задайе N. Напишите программу, 
+// которая выведет все натуральные числа 
+// в промежутке от 1 до N.
+
+Console.Clear();
+string OutPutNumber(int Number)
+
+{
+    if (Number == 1)
+        return 1.ToString();
+    // string Empty = string.Empty;
+    return  OutPutNumber(Number - 1) + ", "+ Number.ToString();
+
+}
+Console.Write("vvedite chislo: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(OutPutNumber(number));
