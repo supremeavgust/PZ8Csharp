6a5681447c57f726e9694c4d35b95ac4d77f7a2c
Stanislav <avgustwot@mail.ru>
2023-04-02 15:32:49 +0300

    Задача 65

---
+++ example65/Program.cs
@@ -0,0 +1,16 @@
+﻿//ЗАДАЧА 65 Задайте значения M и N.
+//Напишите программу, которая выведет
+//все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+string OutPutNumber(int Number1, int Number2)
+
+{
+    if (Number1 == Number2)
+        return Number1.ToString();
+    return OutPutNumber(Number1, Number2 - 1) + ", " + Number2.ToString();
+
+}
+Console.Write("vvedite chislo: ");
+int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.Write(OutPutNumber(numbers[0], numbers[1]));
